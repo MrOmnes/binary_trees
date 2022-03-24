dbd354da4e1975b9cc4df7db2c7c6a48613c7434
MrOmnes <3644@holbertonschool.com>
2022-03-24 11:57:08 +0000

    Add 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Check height of the tree
+ *
+ * @tree: Tree to check
+ * Return: Height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int heightl = 0;
+	int heightr = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	heightl = binary_tree_height(tree->left);
+	heightr = binary_tree_height(tree->right);
+
+	if (heightl >= heightr)
+		return (heightl + 1);
+	else
+		return (heightr + 1);
+}
