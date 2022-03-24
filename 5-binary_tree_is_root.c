4811cd109bd3829e89071caa82c24cca8d212e24
AlpagaSauvage <3645@holbertonschool.com>
2022-03-24 10:47:01 +0000

    add bety style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,15 +2,15 @@
 
 /**
  * binary_tree_is_root - look if is root
- * @node: node 
- * Return: 1 or 0 
+ * @node: node
+ * Return: 1 or 0
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	
+
 	if (node->parent == NULL)
 		return (1);
 	else
