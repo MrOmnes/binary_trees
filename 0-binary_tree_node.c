366079f831d54246da42ecc52a043b08de40813c
MrOmnes <3644@holbertonschool.com>
2022-03-24 09:57:35 +0000

    Fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Create a Binary Tree Node
+ * @parent: Parents node
+ * @value: Value inside the node
+ * Return: node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
